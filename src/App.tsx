071afbb0119135a9e02ce6850e482e27d9e84919
Leandro <ljpcarvalho@gmail.com>
2023-06-06 14:46:37 -0300

    inicial + exercicio 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello World
+      <h1>Gerenciador de senhas</h1>
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,7 @@
+function Form() {
+  return (
+    <div>
+      <label htmlFor="service">Nome do serviço</label>
+    </div>
+  );
+}
